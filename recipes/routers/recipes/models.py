0af6c264523cd51ea1bb8683d390b69e8386e86f
wojtekrichert <33549959+wojtekrichert@users.noreply.github.com>
2025-08-29 13:53:37 +0200

    purge repo

--- recipes/routers/recipes/models.py
+++
@@ -1,9 +0,0 @@
-"""Recipe SQL models."""
-from sqlmodel import Field, SQLModel
-
-
-class Recipe(SQLModel, table=True):
-    """SQL model of single Recipe."""
-
-    id: int | None = Field(default=None, primary_key=True)
-    name: str
